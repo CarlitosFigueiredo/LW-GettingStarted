a5c0006e74905394b962a1c0264d819dca381762
Carlos Yuri Figueiredo <92981178+CarlitosFigueiredo@users.noreply.github.com>
2024-04-27 12:01:44 -0300

    Apenas um sccafoldding para usar o bootstrap 5

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,13 @@
 
 use App\Livewire\Counter;
 use App\Livewire\HelloWorld;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HelloWorld::class);
 
 Route::Get('/counter', Counter::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
